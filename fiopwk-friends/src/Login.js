106586a660ce6476730c7cce6f105cf2b177f5bf
Dajuan Mcdonald <flopwiki@gmail.com>
2020-03-14 18:53:05 -0400

    added axiosWithAuth

--- fiopwk-friends/src/Login.js
+++ fiopwk-friends/src/Login.js
@@ -1,14 +1,52 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Public = () => <Link>Public</Link>
-const Protected = () => <Link>Protected</Link>
+import axios from 'axios';
 
 class Login extends React.Component {
+
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleChanges = event => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
+    login = e => {
+        e.preventDefault();
+        axiosWithAuth()
+        .post()
+        .then()
+        .catch()
+    }
     render() {
         return (
             <div className="login-container">
+                <form onSubmit={this.login}>
+                    <input
+                    onChange={this.handleChanges}
+                    value={this.state.credentials/username} 
+                    name='username'
+                    type='text'>
+                    </input>
+
+                    <input
+                    onChange={this.handleChanges}
+                    value={this.state.credentials.password}
+                    name='password'
+                    type='password'
+                    >
+                    </input>
+
                 <button>Login</button>
+                </form>
 
             </div>
         );
